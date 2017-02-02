fbd0a49a0bc058a541934e606de205c7f6c6888d
code-i-o <NITISH@192.168.0.103>
2017-02-02 22:30:46 +0530

    Comitted Prgm 159 160 and 161

---
+++ src/Prgm161.java
@@ -0,0 +1,13 @@
+// WAP that shows the use of Date class.
+
+import java.util.*;
+//import java.text.*;
+class Prgm161 {
+	public static void main(String args[]){
+		
+//		create date class object- this contains system date and time
+		Date d= new Date();
+		
+//		Format the date and time using 
+	}
+}
