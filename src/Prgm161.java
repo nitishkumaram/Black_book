1134b14911873451db0c430f1e05a91546f98624
code-i-o <NITISH@192.168.0.103>
2017-02-04 19:20:31 +0530

    Commited Prgm161 and 162

--- src/Prgm161.java
+++ src/Prgm161.java
@@ -1,13 +1,22 @@
 // WAP that shows the use of Date class.
 
+
 import java.util.*;
-//import java.text.*;
+import java.text.*;
 class Prgm161 {
 	public static void main(String args[]){
 		
 //		create date class object- this contains system date and time
 		Date d= new Date();
 		
-//		Format the date and time using 
+//		Format the date to medium format and time to short fromat
+		DateFormat fmt= DateFormat.getDateTimeInstance(DateFormat.MEDIUM,DateFormat.SHORT,Locale.UK);
+		
+//		Apply the above format to the date object
+		String str = fmt.format(d);
+		
+//		NOW display the formatted date and time
+		System.out.println(str);
+		
 	}
 }
